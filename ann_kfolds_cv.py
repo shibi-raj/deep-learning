5bbc25b45ca27c7e28526b79549ddd2f84488ff8
Shibi <shibi.1729@gmail.com>
2018-02-13 10:28:33 -0500

    ANN K-folds validation

---
+++ ann_kfolds_cv.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: shibi
+"""
+
+""" Preprocessing """
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# Country
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+# Sex
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+# Dummies for country
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+# Drop redundant dummy
+X = X[:,1:]
+
+
+# Splitting into training and test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+
+
+""" Evaluating & Tuning the NN """
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Build architecture
+def build_classifier():
+    """Function to build NN classifier """
+    classifier = Sequential()
+    classifier.add(Dense(output_dim=6, init='uniform', activation='relu', input_dim=11))
+    classifier.add(Dense(output_dim=6, init='uniform', activation='relu'))
+    classifier.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+
+# K-fold CV
+classifier = KerasClassifier(build_fn=build_classifier, batch_size = 10, nb_epoch = 100)
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10, n_jobs=-1)
